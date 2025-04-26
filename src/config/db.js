6a0bd54f83ac51997af52f3e4471a2f5e0ce32b4
Matthew-uk <esaduviedede@gmail.com>
2025-04-26 15:08:38 +0100

    db conn mended

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://mavo:pa$$w0rd@mavo.vipv5mu.mongodb.net/mavo-app");
+    const conn = await mongoose.connect(
+      'mongodb+srv://mavo:pa$$w0rd@mavo.vipv5mu.mongodb.net/mavo-app',
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
     return conn;
   } catch (error) {
